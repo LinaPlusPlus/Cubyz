82326fa944cdc587e678336101fee076ade02f4f
Lina Plus Plus <TenmaTengo@protonmail.com>
2024-06-28 22:31:58 -0400

    create chat library and add broadcast function

--- src/server/luaLibraries/chat.zig
+++ src/server/luaLibraries/chat.zig
@@ -1,8 +1,32 @@
-const Server = @import("../server.zig");
+const server = @import("root").server;
 const std = @import("std");
 const ziglua = @import("root").ziglua;
-const luasession = @import("root").luasession;
+const LuaSession = @import("root").luasession.LuaSession;
+const LuaState = ziglua.Lua;
+
+const libFunctions = struct {
+	fn broadcast(luaState: *LuaState) i32 {
+		const usageString = "usage: chat.broadcast(messageString)";
+		const message = luaState.toString(-1) catch {
+			_ = luaState.pushBoolean(false);
+			_ = luaState.pushString(usageString);
+			return 2;
+		};
+		// _ = l.pushString("hello, world!");
+		server.sendMessage(message);
+		_ = luaState.pushBoolean(true);
+		return 1;
+	}
+};
 
 pub fn install() anyerror!void {
-    std.log.debug("server had the chat library load, you can remove this message",.{});
+	var luaSession = server.getLuaSession();
+	var luaState = luaSession.getLua();
+
+	luaState.newTable(); // `t`
+
+	luaSession.addNamedFunctionToTable("broadcast",libFunctions.broadcast);
+
+	luaState.setGlobal("chat"); // pop `t`
+	std.debug.assert(luaState.getTop() == 1); //stack should be empty here
 }

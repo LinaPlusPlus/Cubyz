07c794f6a1c4556ec04f1b4532db6dd6b1daf6ff
Lina Plus Plus <TenmaTengo@protonmail.com>
2024-06-29 02:24:47 -0400

    add the ability to bind lua functions to commands
    this is a prototype, it has many HACKs in it

--- src/server/command/_command.zig
+++ src/server/command/_command.zig
@@ -2,12 +2,29 @@ const std = @import("std");
 
 const main = @import("root");
 const User = main.server.User;
+const luaSession = &main.server.luaSession;
+const chat = @import("../luaLibraries/_index.zig").chat;
+const ziglua = main.ziglua;
+//TODO decide if needed const luaState = main.ziglua.Lua;
+
+pub const ZigCommandType = struct {
+	exec: *const fn(args: []const u8, source: *User) void,
+};
+
+
+pub const CommandType = enum { zig,lua };
+
+pub const CommandData = union(CommandType) {
+	zig: ZigCommandType,
+	lua: bool
+};
+
 
 pub const Command = struct {
-	exec: *const fn(args: []const u8, source: *User) void,
 	name: []const u8,
 	description: []const u8,
 	usage: []const u8,
+	commandType: CommandData,
 };
 
 pub var commands: std.StringHashMap(Command) = undefined;
@@ -16,11 +33,13 @@ pub fn init() void {
 	commands = std.StringHashMap(Command).init(main.globalAllocator.allocator);
 	const commandList = @import("_list.zig");
 	inline for(@typeInfo(commandList).Struct.decls) |decl| {
-		commands.put(decl.name, .{
+		commands.put(decl.name, Command{
 			.name = decl.name,
 			.description = @field(commandList, decl.name).description,
 			.usage = @field(commandList, decl.name).usage,
-			.exec = &@field(commandList, decl.name).execute,
+			.commandType = .{ .zig = .{
+				.exec = &@field(commandList, decl.name).execute
+			} }
 		}) catch unreachable;
 	}
 }
@@ -36,7 +55,34 @@ pub fn execute(msg: []const u8, source: *User) void {
 		const result = std.fmt.allocPrint(main.stackAllocator.allocator, "#00ff00Executing Command /{s}", .{msg}) catch unreachable;
 		defer main.stackAllocator.free(result);
 		source.sendMessage(result);
-		cmd.exec(msg[@min(end + 1, msg.len)..], source);
+		switch(cmd.commandType){
+			.zig => |exe| {
+				exe.exec(msg[@min(end + 1, msg.len)..], source);
+			},
+			.lua => |exe| {
+				const lua = luaSession.*.?.luaState.?;
+				lua.pushLightUserdata( &chat.commandsTablePointer );
+				_ = lua.getTable( ziglua.registry_index );
+				_ = lua.pushString( command );
+				_ = lua.getTable( -2 ); //TODO figure out how wo handle type enum here
+
+
+				_ = lua.pushString( msg[@min(end + 1, msg.len)..] ); //pushed the substring
+				lua.pushInteger( source.id ); //TODO HACK: replace with a new uuid scheme
+
+				// pops 2 inputs (+callee), pushes 0 results, 0 = msg_handler disabled?
+				lua.protectedCall( 2, 0, 0 ) catch {
+					// if failed
+					std.log.warn("failed to call this thingy: {s}\n", .{lua.toString(-1) catch unreachable});
+					// Remove the error from the stack and go back to the prompt
+					lua.pop(1);
+					@panic("failed to call the thingy"); //TODO HACK moving to better error infrastructure
+				};
+				_ = exe;
+				//lua.getTable()
+				//command
+			}
+		}
 	} else {
 		const result = std.fmt.allocPrint(main.stackAllocator.allocator, "#ff0000Unrecognized Command \"{s}\"", .{command}) catch unreachable;
 		defer main.stackAllocator.free(result);
--- src/server/luaLibraries/chat.zig
+++ src/server/luaLibraries/chat.zig
@@ -2,12 +2,17 @@ const server = @import("root").server;
 const std = @import("std");
 const ziglua = @import("root").ziglua;
 const LuaSession = @import("root").luasession.LuaSession;
+const Userdata = @import("root").luasession.Userdata;
+const command = @import("../command/_command.zig");
 const LuaState = ziglua.Lua;
+
+
+pub var commandsTablePointer: Userdata = .None;
 
 const libFunctions = struct {
 	fn broadcast(luaState: *LuaState) i32 {
 		const usageString = "usage: chat.broadcast(messageString)";
-		const message = luaState.toString(-1) catch {
+		const message = luaState.toString(1) catch {
 			_ = luaState.pushBoolean(false);
 			_ = luaState.pushString(usageString);
 			return 2;
@@ -17,16 +22,70 @@ const libFunctions = struct {
 		_ = luaState.pushBoolean(true);
 		return 1;
 	}
+	fn regCommand(luaState: *LuaState) i32 {
+		//HACK allowing overriting bind, cannot safely register twice
+		// args: name [1], function [2]
+		if ( luaState.isNil(2) ) {
+			std.log.warn("failed get function: {s}\n", .{luaState.toString(-1) catch unreachable});
+			@panic("lua fail"); //HACK sloppy handling
+		}
+		const name = luaState.toString(1) catch {
+			std.log.warn("failed get name: {s}\n", .{luaState.toString(-1) catch unreachable});
+			@panic("lua fail"); //HACK sloppy handling
+		};
+		luaState.pushLightUserdata( &commandsTablePointer ); // push `testForK`
+		_ = luaState.getTable( ziglua.registry_index ); // push `testForV` pop `testForK`
+		//TODO add checking here
+		_ = luaState.pushString(name);
+		luaState.pushValue( 2 );
+		luaState.setTable( -3 ); //pop name string and
+
+		//slopply clear the stack HACK
+		luaState.pop( -1 );
+		luaState.pop( -1 );
+		luaState.pop( -1 );
+
+		_ = command.commands.put(name,.{
+			.name = name,
+			.description = "lua added this",
+			.usage = "I really dont know",
+			.commandType = .{ .lua = true },
+		}) catch {
+			@panic("lua fail: insert command"); //HACK sloppy handling
+		};
+		return 0;
+	}
 };
 
 pub fn install() anyerror!void {
-	var luaSession = server.getLuaSession();
-	var luaState = luaSession.getLua();
+	var luaSession = server.luaSession;
+	var luaState = luaSession.?.luaState.?;
+
+	if (commandsTablePointer == .None){
+		// use this as a variable to satisfy compiler
+		commandsTablePointer = .Unique;
+	}
 
 	luaState.newTable(); // `t`
 
-	luaSession.addNamedFunctionToTable("broadcast",libFunctions.broadcast);
+	luaSession.?.addNamedFunctionToTable("broadcast",libFunctions.broadcast);
+	luaSession.?.addNamedFunctionToTable("registerCommand",libFunctions.regCommand);
 
 	luaState.setGlobal("chat"); // pop `t`
 	std.debug.assert(luaState.getTop() == 1); //stack should be empty here
+
+	//TODO replace this boilerplate with either a function or a simpler version
+	while (true) { //here for its ability to break;
+		luaState.pushLightUserdata( &commandsTablePointer ); // push `testForK`
+		_ = luaState.getTable( ziglua.registry_index ); // push `testForV` pop `testForK`
+		const isNil = luaState.isNil(-1);
+		luaState.pop(-1); //pop `testForV`
+		if( isNil ) {
+			luaState.pushLightUserdata( &commandsTablePointer ); // push `k`
+			luaState.newTable(); // push `v`
+			luaState.setTable( ziglua.registry_index ); //pops `k` and `v`
+		} else {
+			break;
+		}
+	}
 }

3afbcfc22ce589caf04d98d581fedeb4921237e1
Lina Plus Plus <TenmaTengo@protonmail.com>
2024-06-29 03:16:10 -0400

    fix formatting

--- src/luasession/libraries/events.zig
+++ src/luasession/libraries/events.zig
@@ -13,53 +13,53 @@ const FnReg = ziglua.FnReg;
 var eventTablePointer: Userdata = .None;
 
 const libFunctions = struct {
-    fn helloWorld(l: *LuaState) i32 {
-        _ = l.pushString("hello, world!");
-        return 1;
-    }
+	fn helloWorld(l: *LuaState) i32 {
+		_ = l.pushString("hello, world!");
+		return 1;
+	}
 };
 
 
 pub fn install(session: *LuaSession, platform: Platform) anyerror!void {
-    if (eventTablePointer == .None){
-        // use this as a variable to satisfy compiler
-        eventTablePointer = .Unique; 
-    }
-    _ = platform; // we dont care, install everywhere
-    const luaState: *LuaState = session.luaState.?;
-    
-    // WARNING the luac API will push and pop 
-    // values in ways that are not self evident
-    // read the documentation for luac functions before use
-    
-    while (true) { //here for its ability to break;
-        std.debug.assert(luaState.getTop() == 1); //stack should be empty here
-        
-        //BEGIN install internal table
-        luaState.pushLightUserdata( &eventTablePointer ); // push `testForK`
-        _ = luaState.getTable( ziglua.registry_index ); // push `testForV` pop `testForK`
-        const isNil = luaState.isNil(-1);
-        luaState.pop(-1); //pop `testForV`
-        if( isNil ) {
-            luaState.pushLightUserdata( &eventTablePointer ); // push `k`
-            luaState.newTable(); // push `v`
-            luaState.setTable( ziglua.registry_index ); //pops `k` and `v`
-        } else {
-            break;
-        }
-        //END install internal table
-        
-        std.debug.assert(luaState.getTop() == 1); //stack should be empty here
-        
-        //BEGIN add functions to library
-        luaState.newTable(); // `lib`
-        
-        session.addNamedFunctionToTable("helloWorld",libFunctions.helloWorld);
-        
-        luaState.setGlobal("events"); //pops `lib`
-        //END add functions to library
-        
-        std.debug.assert(luaState.getTop() == 1); //stack should be empty here
-        break; //finished install
-    }
+	if (eventTablePointer == .None){
+		// use this as a variable to satisfy compiler
+		eventTablePointer = .Unique;
+	}
+	_ = platform; // we dont care, install everywhere
+	const luaState: *LuaState = session.luaState.?;
+
+	// WARNING the luac API will push and pop
+	// values in ways that are not self evident
+	// read the documentation for luac functions before use
+
+	while (true) { //here for its ability to break;
+		std.debug.assert(luaState.getTop() == 1); //stack should be empty here
+
+		//BEGIN install internal table
+		luaState.pushLightUserdata( &eventTablePointer ); // push `testForK`
+		_ = luaState.getTable( ziglua.registry_index ); // push `testForV` pop `testForK`
+		const isNil = luaState.isNil(-1);
+		luaState.pop(-1); //pop `testForV`
+		if( isNil ) {
+			luaState.pushLightUserdata( &eventTablePointer ); // push `k`
+			luaState.newTable(); // push `v`
+			luaState.setTable( ziglua.registry_index ); //pops `k` and `v`
+		} else {
+			break;
+		}
+		//END install internal table
+
+		std.debug.assert(luaState.getTop() == 1); //stack should be empty here
+
+		//BEGIN add functions to library
+		luaState.newTable(); // `lib`
+
+		session.addNamedFunctionToTable("helloWorld",libFunctions.helloWorld);
+
+		luaState.setGlobal("events"); //pops `lib`
+		//END add functions to library
+
+		std.debug.assert(luaState.getTop() == 1); //stack should be empty here
+		break; //finished install
+	}
 }
--- src/luasession/session.zig
+++ src/luasession/session.zig
@@ -6,68 +6,67 @@ const modList = @import("libraries/_index.zig");
 const modLength = @typeInfo(modList).Struct.decls.len;
 
 pub const Error = error {
-    DoubleInit,
-    DoubleDeinit, //or not inited
-    Error,
-    IOError,
+	DoubleInit,
+	DoubleDeinit, //or not inited
+	Error,
+	IOError,
 };
 
 pub const Platform = enum {
-    Server,
-    Client,
+	Server,
+	Client,
 };
 
 pub const Userdata = union(enum) {
-    None, // an "empty" userdata
-    Unique, // a strictly unique userdata
-    //WIP
+	None, // an "empty" userdata
+	Unique, // a strictly unique userdata
+	//WIP
 };
 
 pub const LuaSession = struct {
-    initialized: bool = false,
-    eventSystem: bool = false, //represents initialized check and anyopaque table key
-    
-    luaState: ?*LuaState = null,
-    
-    
-    pub fn init(self: *LuaSession, luaState: *LuaState) anyerror!void {
-        std.debug.assert(self.*.initialized == false);
-        self.*.initialized = true;
-        self.*.luaState = luaState;
-    }
+	initialized: bool = false,
+	eventSystem: bool = false, //represents initialized check and anyopaque table key
 
-    pub fn installCommonSystems(self: *LuaSession, targetPlatform: Platform) anyerror!void {
-        std.debug.assert(self.*.initialized == true);
-        const luaState = self.*.luaState.?;
-        std.log.info("install lua library: std",.{});
-        luaState.openLibs();
-            
-        inline for(@typeInfo(modList).Struct.decls) |decl| {
-            std.log.info("install lua library: {s}",.{decl.name});
-            _ = try @field(modList, decl.name).install(self,targetPlatform);
-        }
-    }
+	luaState: ?*LuaState = null,
 
-    //TODO HACK file string is a const, change to *string or zig equivlant
-    //TODO from server.init(...) inline into function;
-    pub fn loadContextlessFile(self: *LuaSession) anyerror!void {
-        std.debug.assert(self.*.initialized == true);
-        const luaState = self.*.luaState orelse unreachable;
-        
-        std.debug.assert(self.*.initialized == true);
-        try luaState.loadFile("server_init_contextless.lua",.text);
-        // 0 inputs 0, returns, index of 0 for some reason
-        // argument 3 is `0` in this simmlar function, blindly cloning
-        // https://github.com/natecraddock/ziglua/blob/a7cf85fb871a95a46d4222fe3abdd3946e3e0dab/src/lib.zig#L2521
-        try luaState.protectedCall(0,0,0);
-    }
-    
-    pub fn addNamedFunctionToTable(self: *LuaSession, name: []const u8, func: ziglua.ZigFn) void {
-        const luaState = self.*.luaState.?;
-        _ = luaState.pushString(name); //push `k`
-        luaState.pushFunction(ziglua.wrap(func)); // push `v`
-        //applies to whatever is on the top of the stack
-        luaState.setTable( -3 ); //pops `k` and `v`
-    }
+
+	pub fn init(self: *LuaSession, luaState: *LuaState) anyerror!void {
+		std.debug.assert(self.*.initialized == false);
+		self.*.initialized = true;
+		self.*.luaState = luaState;
+	}
+
+	pub fn installCommonSystems(self: *LuaSession, targetPlatform: Platform) anyerror!void {
+		std.debug.assert(self.*.initialized == true);
+		const luaState = self.*.luaState.?;
+		std.log.info("install lua library: std",.{});
+		luaState.openLibs();
+
+		inline for(@typeInfo(modList).Struct.decls) |decl| {
+			std.log.info("install lua library: {s}",.{decl.name});
+			_ = try @field(modList, decl.name).install(self,targetPlatform);
+		}
+	}
+
+	//TODO HACK file string is a const, change to *string or zig equivlant
+	//TODO from server.init(...) inline into function;
+	pub fn loadContextlessFile(self: *LuaSession) anyerror!void {
+		std.debug.assert(self.*.initialized == true);
+		const luaState = self.*.luaState orelse unreachable;
+
+		std.debug.assert(self.*.initialized == true);
+		try luaState.loadFile("server_init_contextless.lua",.text);
+		// 0 inputs 0, returns, index of 0 for some reason
+		// argument 3 is `0` in this simmlar function, blindly cloning
+		// https://github.com/natecraddock/ziglua/blob/a7cf85fb871a95a46d4222fe3abdd3946e3e0dab/src/lib.zig#L2521
+		try luaState.protectedCall(0,0,0);
+	}
+
+	pub fn addNamedFunctionToTable(self: *LuaSession, name: []const u8, func: ziglua.ZigFn) void {
+		const luaState = self.*.luaState.?;
+		_ = luaState.pushString(name); //push `k`
+		luaState.pushFunction(ziglua.wrap(func)); // push `v`
+		//applies to whatever is on the top of the stack
+		luaState.setTable( -3 ); //pops `k` and `v`
+	}
 };
-
